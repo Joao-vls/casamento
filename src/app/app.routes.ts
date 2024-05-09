d472efe3611926d22f6170c0f1cfdc91fbb5808c
joao-vls <joa.v55j@gmail.com>
2024-05-09 15:37:59 -0300

    comentarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PlanejamentoComponent } from './pages/planejamento/planejamento.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 export const routes: Routes = [
     {'path':'',component:HomeComponent},
     {'path':'planejamento',component:PlanejamentoComponent},
+    {'path':'cadastro',component:CadastroComponent},
     {'path':'**',component:HomeComponent}
 ];
--- src/app/shared/form-cadastro-casamento/form-cadastro-casamento.component.ts
+++ src/app/shared/form-cadastro-casamento/form-cadastro-casamento.component.ts
@@ -4,31 +4,29 @@ import { CadastroService } from '../../services/cadastro.service';
 import { CommonModule } from '@angular/common';
 import { LocaisService } from '../../services/locais.service';
 import { Locais } from '../../models/locais';
-import { ServicosService } from '../../services/servicos.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-cadastro-casamento',
-  standalone: true,
+  standalone: true, // Não está definido na documentação do Angular. Possível erro?
   templateUrl: './form-cadastro-casamento.component.html',
   styleUrl: './form-cadastro-casamento.component.css',
-  imports: [ReactiveFormsModule, FormsModule, CommonModule]
+  imports: [ReactiveFormsModule, FormsModule, CommonModule] // Importa os módulos necessários
 })
-
-
 export class FormCadastroCasamentoComponent implements OnInit {
+  casamento!: FormGroup; // Formulário de casamento
+  activePadrinhos: boolean = true; // Define se os padrinhos estão ativos ou não
+  minData: string; // Data mínima para seleção no calendário
+  maxData: string; // Data máxima para seleção no calendário
+  localizacao!: Locais[]; // Array de locais
+  cidadeSelecionada!: string; // Cidade selecionada
+  cidadesRepet:string[]=[]; // Array de cidades repetidas
+  locais!: Locais[]; // Array de locais
+  @Input()  valorTotal!:number; // Valor total do casamento recebido como entrada
+  @Output() newItemEvent = new EventEmitter<string>(); // Evento para adicionar novo item
+  @Output() newServico = new EventEmitter<boolean>(); // Evento para ativar serviços
 
-  casamento!: FormGroup;
-  activePadrinhos: boolean = true;
-  minData: string;
-  maxData: string;
-  localizacao!: Locais[]
-  cidadeSelecionada!: string;
-  cidadesRepet:string[]=[];
-  locais!: Locais[];
-  @Input()  valorTotal!:number;
-  @Output() newItemEvent = new EventEmitter<string>();
-  @Output() newServico = new EventEmitter<boolean>();
+  // Métodos getters para acessar os controles do formulário
   get noivos() {
     return this.casamento.get('noivos') as FormArray;
   }
@@ -44,13 +42,12 @@ export class FormCadastroCasamentoComponent implements OnInit {
   get quantidadeConvidados() {
     return this.casamento.get('quantidadeConvidados') as FormControl;
   }
-
   get cidade() {
     return this.casamento.get('cidade') as FormControl;
   }
 
-  
-  constructor(private formService: CadastroService, private locaisg: LocaisService,private dataService: ServicosService) {
+  constructor(private formService: CadastroService, private locaisg: LocaisService,private router: Router) {
+    // Inicializa a data mínima e máxima para seleção no calendário
     const currentDat = new Date();
     currentDat.setDate(currentDat.getDate() + 7);
     this.minData = currentDat.toISOString().split('T')[0];
@@ -58,57 +55,66 @@ export class FormCadastroCasamentoComponent implements OnInit {
     currentDate.setFullYear(currentDate.getFullYear() + 100);
     this.maxData = currentDate.toISOString().split('T')[0];
   }
+
+  // Desativa os padrinhos e remove os inputs dinâmicos de padrinhos
   disablePadrinhos() {
     this.activePadrinhos = false;
     for (let i = this.padrinhos.length - 1; i >= 0; i--) {
       this.removeInput(i, 'padrinhos');
     }
-
   }
+
   ngOnInit() {
+    // Inicializa o formulário de casamento e adiciona inputs dinâmicos para noivos e padrinhos
     this.casamento = this.formService.getDynamicForm();
     this.formService.addDynamicInput('noivos', this.casamento);
     this.formService.addDynamicInput('noivos', this.casamento);
     this.formService.addDynamicInput('padrinhos', this.casamento);
     this.formService.addDynamicInput('padrinhos', this.casamento);
-    this.locaisOptions()
-
+    this.locaisOptions(); // Carrega as opções de locais
   }
 
+  // Carrega as opções de locais
   locaisOptions() {
     this.locaisg.getLocais().subscribe((response: Locais[]) => {
       this.locais = response;
-      this.municipioExiste()
+      this.municipioExiste();
     });
   }
+
+  // Seleciona a opção de cidade
   selectOpitionCidade() {
     this.localizacao = this.locais.filter(loc => {
-      return loc.municipio == this.cidade.value
-    })
+      return loc.municipio == this.cidade.value;
+    });
   }
-  selectOpitionLocal() {
-    this.NewLimit(this.local.value)
-    if (this.local.value > this.quantidadeConvidados.value) {
 
-    }
+  // Seleciona a opção de local e ajusta o limite de convidados
+  selectOpitionLocal() {
+    this.NewLimit(this.local.value);
   }
+
+  // Adiciona um novo input dinâmico
   addInput(nome: string) {
     this.formService.addDynamicInput(nome, this.casamento);
   }
+
+  // Atualiza o limite de convidados com base no local selecionado
   NewLimit(i: number) {
     this.quantidadeConvidados.setValidators([Validators.max(i)]);
     this.quantidadeConvidados.updateValueAndValidity();
-
   }
+
+  // Remove um input dinâmico
   removeInput(index: number, nameInput: string) {
     if (this.casamento.value.noivos.length > 2 && nameInput == 'noivos') {
       this.formService.removeDynamicInput(this.casamento, index, nameInput);
     } if ((this.casamento.value.padrinhos.length > 2 && nameInput == 'padrinhos') || !this.activePadrinhos) {
-
       this.formService.removeDynamicInput(this.casamento, index, nameInput);
     }
   }
 
+  // Obtém o FormControl
   getFormControl(formControl: AbstractControl, controlName: string): FormControl {
     if (formControl instanceof FormGroup) {
       const control = formControl.get(controlName);
@@ -117,23 +123,28 @@ export class FormCadastroCasamentoComponent implements OnInit {
     return new FormControl();
   }
 
+  // Manipula o envio do formulário
   onSubmit() {
-    // Lógica para manipular o envio do formulário
     if (this.casamento.valid) {
-      const informacoes={...this.casamento.value,valorTotal : this.valorTotal}
+      const informacoes={...this.casamento.value,valorTotal : this.valorTotal};
       console.log('Formulário válido, enviar dados...', informacoes);
+      this.redirectCadastro();
     } else {
       console.log('Formulário inválido, corrija os erros.');
     }
   }
 
+  // Adiciona um novo item
   addNewItem() {
     this.newItemEvent.emit(this.casamento.value.noivos);
   }
+
+  // Ativa serviços
   activeServicos(){
-    this.newServico.emit(true)
+    this.newServico.emit(true);
   }
 
+  // Verifica se o município já existe
   municipioExiste() {
     for (let index = 0; index < this.locais.length; index++) {
       let bo:boolean=true;
@@ -148,22 +159,28 @@ export class FormCadastroCasamentoComponent implements OnInit {
       } 
     }
 
-    blockExponent(e:KeyboardEvent){
-      const charCode = e.key.charCodeAt(0);
-      if (e.key === 'e' || e.key === 'E' || e.key==='+'|| e.key==='-'|| e.key==='¨') {
-        e.preventDefault();
-      }
+  // Bloqueia a digitação de caracteres inválidos
+  blockExponent(e:KeyboardEvent){
+    const charCode = e.key.charCodeAt(0);
+    if (e.key === 'e' || e.key === 'E' || e.key==='+'|| e.key==='-'|| e.key==='¨') {
+      e.preventDefault();
     }
-    sanitizeInput(event: any): void {
-      const input = event.target;
-      let value = input.value;
-  
-      // Remove acentos, caracteres especiais e letras do valor digitado
-      value = value.replace(/[^0-9]/g, '');
-  
-      // Atualiza o valor do campo de entrada
-      input.value = value;
-    }
+  }
 
-    
+  // Remove caracteres inválidos de um campo de entrada
+  sanitizeInput(event: any): void {
+    const input = event.target;
+    let value = input.value;
+
+    // Remove acentos, caracteres especiais e letras do valor digitado
+    value = value.replace(/[^0-9]/g, '');
+
+    // Atualiza o valor do campo de entrada
+    input.value = value;
+  }
+
+  // Redireciona para a página de cadastro
+  redirectCadastro(){
+    this.router.navigate(['/cadastro']);
+  }  
 }
